3a1dec7c0837adde5981ddde9b92b9807f3bf5d9
renopeno <renato.mandic@gmail.com>
2025-01-27 16:28:42 +0100

    lenis - gsap fix

--- global/lenis-config.js
+++ global/lenis-config.js
@@ -1,25 +1,30 @@
 import Lenis from '@studio-freight/lenis';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+// Registriraj ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
 
 function initLenis () {
-  const lenis = new Lenis({
-    duration: 0.65, // Tromost scrolla
-    easing: (t) => 1 - Math.pow(1 - t, 3), // Blago ubrzanje i usporavanje
-    smooth: true, // Omogući smooth scroll
-    smoothTouch: true, // Poboljšano scrollanje za touch uređaje
-});
+    const lenis = new Lenis({
+        duration: 0.65, // Tromost scrolla
+        easing: (t) => 1 - Math.pow(1 - t, 3), // Blago ubrzanje i usporavanje
+        smooth: true, // Omogući smooth scroll
+        smoothTouch: true, // Poboljšano scrollanje za touch uređaje
+    });
 
-// Pokretanje animacije s requestAnimationFrame
-function raf(time) {
-  lenis.raf(time); // Pozovi Lenis na svaku promjenu frejmova
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+    // Pokretanje animacije s requestAnimationFrame
+    function raf(time) {
+        lenis.raf(time); // Pozovi Lenis na svaku promjenu frejmova
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
 
-// Povezivanje GSAP-a i Lenis-a
-gsap.ticker.add((time) => lenis.raf(time));
+    // Povezivanje GSAP-a i Lenis-a
+    gsap.ticker.add((time) => lenis.raf(time));
 
-// Ažuriranje ScrollTrigger-a za Lenis
-lenis.on('scroll', ScrollTrigger.update);
+    // Ažuriranje ScrollTrigger-a za Lenis
+    lenis.on('scroll', ScrollTrigger.update);
 }
 
 export default initLenis;
