155dbec0875f34388523439dce042725c5ebe6ec
renopeno <renato.mandic@gmail.com>
2025-01-27 19:20:18 +0100

    gsap plugins instalacija na drugi nacin

--- global/gsap-plugins.js
+++ global/gsap-plugins.js
@@ -1,5 +1,1 @@
-function initGsapPlugins() {
-    gsap.registerPlugin(Flip, ScrollTrigger, CustomEase); // Globalni `gsap` dostupan putem CDN-a
-  }
-  
-initGsapPlugins();
+gsap.registerPlugin(Flip, ScrollTrigger, CustomEase);
