10f895b9faae73bf24fa745fd1c2b7af1491914c
renopeno <renato.mandic@gmail.com>
2025-01-27 16:03:40 +0100

    barba dodan u index.global.js

--- global/index.js
+++ global/index.js
@@ -4,3 +4,27 @@ import './footer-gsap.js';
 import './gsap-plugins.js';
 import './lenis.js';
 import './links-hover-effect.js';
+
+function loadBarba() {
+    if (document.querySelector('script[src="https://tomislav-vukic-web-team-renopeno.vercel.app/barba.bundle.js"]')) {
+        console.log('Barba.js već učitan');
+        return;
+    }
+    const script = document.createElement('script');
+    script.src = "https://tomislav-vukic-web-team-renopeno.vercel.app/barba.bundle.js"; // URL Barba.js bundle-a
+    script.defer = true;
+    document.body.appendChild(script);
+  
+    script.onload = () => {
+      console.log('Barba.js loaded successfully!');
+    };
+  
+    script.onerror = () => {
+      console.error('Failed to load Barba.js');
+    };
+}
+  
+  // Pozovi loadBarba nakon što se sve ostale funkcije inicijaliziraju
+  window.addEventListener('load', () => {
+    loadBarba(); // Dinamički učitaj Barba.js
+  });
