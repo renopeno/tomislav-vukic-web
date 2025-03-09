d70f4c12bf18c90ba9af3e169fbe735e221303f4
renopeno <renato.mandic@gmail.com>
2025-03-09 22:32:48 +0100

    dark console log

--- global/dark-mode.js
+++ global/dark-mode.js
@@ -1,33 +1,30 @@
 function initDarkMode() {
   const body = document.body;
-  const html = document.documentElement;
   const themeSwitcher = document.querySelector('.nav-theme-switcher');
 
-  // Primijeni Dark Mode odmah na početku
+  // Prikaz u konzoli koji mode je trenutno učitan
+  const currentMode = localStorage.getItem('dark-mode') === 'enabled' ? 'Dark Mode' : 'Light Mode';
+  console.log(`Initial mode: ${currentMode}`);
+
+  //  Primijeni Dark Mode odmah na početku
   if (localStorage.getItem('dark-mode') === 'enabled') {
-    html.classList.add('ui-dark-mode'); // Dodajemo na <html> umjesto samo na <body>
     body.classList.add('ui-dark-mode');
   }
 
-  // Osiguraj da se Dark Mode pravilno prebacuje pri kliku na switcher
+  //  Osiguraj da se Dark Mode pravilno prebacuje pri kliku na switcher
   if (themeSwitcher) {
     themeSwitcher.addEventListener('click', () => {
       if (body.classList.contains('ui-dark-mode')) {
-        html.classList.remove('ui-dark-mode');
         body.classList.remove('ui-dark-mode');
         localStorage.setItem('dark-mode', 'disabled');
+        console.log("Switched to Light Mode");
       } else {
-        html.classList.add('ui-dark-mode');
         body.classList.add('ui-dark-mode');
         localStorage.setItem('dark-mode', 'enabled');
+        console.log("Switched to Dark Mode");
       }
     });
   }
-}
-
-// Izvrši dodavanje klase odmah pri učitavanju kako bi se izbjegao bljesak
-if (localStorage.getItem('dark-mode') === 'enabled') {
-  document.documentElement.classList.add('ui-dark-mode');
 }
 
 initDarkMode();
