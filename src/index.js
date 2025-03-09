c6c9a8096b7b030b206bed6d744b3ec283b928e0
renopeno <renato.mandic@gmail.com>
2025-03-09 23:16:34 +0100

    pokusaj prebacivanja na vite i nova folder struktura

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Globalne skripte
+import './global/lenis-config.js';
+import './global/dark-mode.js';
+import './global/footer-gsap.js';
+import './global/links-hover-effect.js';
+import './global/ios-safari-fix.js';
+import './global/navigation-cms-page-fix.js';
+
+// Home page skripte
+import './home/hero.js';
+import './home/about.js';
+import './home/highlights.js';
+import './home/categories.js';
+
+// Work page skripte
+import './work/grid.js';
+import './work/photo-modal.js';
+
+// About page
+import './about/about-page.js';
