fbfc1ed46a2e40ba238c5c686beba5927c1af07b
renopeno <renato.mandic@gmail.com>
2025-03-09 22:29:16 +0100

    dark mode fix?

--- global/dark-mode.js
+++ global/dark-mode.js
@@ -1,41 +1,23 @@
-// Primijeni Dark Mode odmah na HTML element prije bilo čega drugog
-if (localStorage.getItem('dark-mode') === 'enabled') {
-  document.documentElement.classList.add('ui-dark-mode');
-}
-
 function initDarkMode() {
   const body = document.body;
   const themeSwitcher = document.querySelector('.nav-theme-switcher');
 
-  //  Primijeni Dark Mode odmah na početku
+  // Primijeni Dark Mode odmah na početku
   if (localStorage.getItem('dark-mode') === 'enabled') {
     body.classList.add('ui-dark-mode');
   }
 
-  //  Osiguraj da se Dark Mode pravilno prebacuje pri kliku na switcher
+  // Osiguraj da se Dark Mode pravilno prebacuje pri kliku na switcher
   if (themeSwitcher) {
     themeSwitcher.addEventListener('click', () => {
       if (body.classList.contains('ui-dark-mode')) {
         body.classList.remove('ui-dark-mode');
-        document.documentElement.classList.remove('ui-dark-mode');
         console.log("Removed .ui-dark-mode from body");
         localStorage.setItem('dark-mode', 'disabled');
       } else {
         body.classList.add('ui-dark-mode');
-        document.documentElement.classList.add('ui-dark-mode');
         console.log("Added .ui-dark-mode to body");
         localStorage.setItem('dark-mode', 'enabled');
-      }
-    });
-  }
-
-  //  Osiguraj primjenu pri svakoj Barba.js tranziciji
-  if (window.barba) {
-    barba.hooks.beforeEnter(() => {
-      if (localStorage.getItem('dark-mode') === 'enabled') {
-        document.body.classList.add('ui-dark-mode');
-      } else {
-        document.body.classList.remove('ui-dark-mode');
       }
     });
   }
